b58c656ad62bb4a01bec90a0379d0d8db395fbfb
JLizak <152606639+JLizak@users.noreply.github.com>
2024-03-22 00:53:49 +0100

    Delete src directory

--- src/robocik_zadanie/robocik_zadanie/settings.py
+++
@@ -1,6 +0,0 @@
-class Settings:
-    def __init__(self):
-        self.screen_width = 600
-        self.screen_height = 400
-        self.bg_color = (14, 152, 186)
-        self.submarine_speed = 5.0 
